a5a1f1f7f1359c6a8b5202481982d4880344c858
IMiMineDigital <immmobileapps@gmail.com>
2019-10-11 12:00:35 +0530

    1 commit

---
+++ FarewyLibrary.framework/Headers/FarewyLibrary.h
@@ -0,0 +1,20 @@
+//
+//  FarewyLibrary.h
+//  FarewyLibrary
+//
+//  Created by kamlesh prajapati on 6/09/19.
+//  Copyright © 2019 kamlesh prajapati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GlobalClass.h"
+
+//! Project version number for FarewyLibrary.
+FOUNDATION_EXPORT double FarewyLibraryVersionNumber;
+
+//! Project version string for FarewyLibrary.
+FOUNDATION_EXPORT const unsigned char FarewyLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FarewyLibrary/PublicHeader.h>
+
+
---
+++ FarewyLibrary.framework/Headers/GlobalClass.h
@@ -0,0 +1,17 @@
+//
+//  GlobalClass.h
+//  FarewyLibrary
+//
+//  Created by kamlesh prajapati on 6/09/19.
+//  Copyright © 2019 kamlesh prajapati. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GlobalClass : NSObject
+-(void)calling;
+@end
+
+NS_ASSUME_NONNULL_END
